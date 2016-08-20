d556074b7a87d6452447eec8afd8b889aa1e5b90
Kevan Hollbach <khollbach@gmail.com>
2016-08-20 09:39:41 -0400

    Add row- and column-specific constraint functions.

--- alpha_o.py
+++ alpha_o.py
@@ -1,4 +1,5 @@
 from file_io import *
+from puzzle_logic import *
 
 def main():
     row_hints, column_hints = read_hints('examples/problem1.txt')
@@ -9,6 +10,14 @@ def main():
     board = read_solution('examples/solution1.txt')
 
     print(board)
+
+    for i in range(len(board)):
+        if not satisfies_row_constraint(board, i, row_hints[i]):
+            print('row', i, "doesn't match.")
+
+    for j in range(len(board[0])):
+        if not satisfies_column_constraint(board, j, column_hints[j]):
+            print('column', j, "doesn't match.")
 
     write_solution(board, 'tmp.txt')
 
--- puzzle_logic.py
+++ puzzle_logic.py
@@ -19,3 +19,17 @@ def satisfies_constraint(row_or_column, expected_blocks):
         blocks.append(current_block_size)
 
     return blocks == expected_blocks
+
+def satisfies_row_constraint(board, row_index, expected_blocks):
+    '''([[WHITE|BLACK, ...], ...], int, [int, ...]) -> bool
+    Return True iff the row has a coloring that agrees with the 'hint'.
+    '''
+    row = board[row_index]
+    return satisfies_constraint(board[row_index], expected_blocks)
+
+def satisfies_column_constraint(board, column_index, expected_blocks):
+    '''([[WHITE|BLACK, ...], ...], int, [int, ...]) -> bool
+    Return True iff the column has a coloring that agrees with the 'hint'.
+    '''
+    column = [board[i][column_index] for i in range(len(board))]
+    return satisfies_constraint(column, expected_blocks)
